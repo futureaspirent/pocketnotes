57e3ea4f514ce523f0bc7801ca2cfd194d04689f
Swathi Mudi <swathimudi83@gmail.com>
2025-10-29 10:49:27 +0530

    Fix illustration image path

--- src/components/NotesSection.js
+++ src/components/NotesSection.js
@@ -41,7 +41,11 @@ const NotesSection = ({ currentGroupId, groups }) => {
   if (!currentGroupId) {
     return (
       <main className="notes-section empty">
-        <img src={Illustration} alt="Pocket Notes Illustration" className="illustration" />
+      <img 
+            src={Illustration} 
+            alt="Pocket Notes Illustration" 
+           className="illustration" 
+          />
         <h2>Pocket Notes</h2>
         <p>Send and receive messages without keeping your phone online.</p>
         <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
